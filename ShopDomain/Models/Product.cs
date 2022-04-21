9f7560e39e54ce540b742fac50ee460a4d38aedb
alexangria1992 <alex040892@gmail.com>
2022-04-21 21:48:00 +1200

    Installed Entity Framework Core & set up Database

--- ShopDomain/Models/Product.cs
+++ ShopDomain/Models/Product.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ShopDomain.Models
+namespace Shop.Domain.Models
 {
-    class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
     }
 }
